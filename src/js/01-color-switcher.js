5a2994782f5b9d953db08e6ab3ca22df8a9bf29c
Maryna Ushakova <ginger4795@gmail.com>
2022-07-15 18:19:43 +0300

    task 01 added

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const bodyEl = document.querySelector('body');
+
+let intervalId = null;
+stopBtn.disabled = true;
+
+startBtn.addEventListener('click', () => {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+
+  intervalId = setInterval(() => {
+    bodyEl.style.backgroundColor = `${getRandomHexColor()}`;
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(intervalId);
+});
