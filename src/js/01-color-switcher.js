71b04cb1d443994f94d01f58dbbc7dfef70dc2b0
Maryna Ushakova <ginger4795@gmail.com>
2022-09-17 00:13:30 +0300

    task 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,45 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const bodyEl = document.querySelector('body');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let timerId;
+stopBtn.disabled = true;
+
+const changeBodyColor = () => {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+};
+
+const onStartBtnClick = () => {
+  timerId = setInterval(changeBodyColor, 1000);
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+};
+
+const onStopBtnClick = () => {
+  clearInterval(timerId);
+};
+
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+
+//
+//
+// let intervalId = null;
+// stopBtn.disabled = true;
+
+//   intervalId = setInterval(() => {
+//     bodyEl.style.backgroundColor = getRandomHexColor();
+//   }, 1000);
+// });
+
+// startBtn.addEventListener('click', () => {
+//   startBtn.disabled = true;
+//   stopBtn.disabled = false;
+
+// stopBtn.addEventListener('click', () => {
+//   clearInterval(intervalId);
+// });
