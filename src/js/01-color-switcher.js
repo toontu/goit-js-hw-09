e1a493f13717f237f387cf708b345787aeccc36a
Maryna Ushakova <ginger4795@gmail.com>
2022-09-18 19:11:39 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -6,7 +6,7 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-let timerId;
+let timerId = null;
 stopBtn.disabled = true;
 
 const changeBodyColor = () => {
